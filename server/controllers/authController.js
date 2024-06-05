843c1d4d7c8e65cf3a507527a620e9a01f969ca2
npcwalison <gwalisonls@gmail.com>
2024-06-05 16:39:26 -0300

    ...

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,17 +1,19 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require('../models/userModel');
-const createError = require('../utils/appError');
+const User = require("../models/userModel");
+const createError = require("../utils/appError");
 
 // REGISTER USER
 exports.signup = async (req, res, next) => {
-    try{
+    try {
         // faz uma busca especifica pelo email
-        const user = await User.findOne({ email: req.body.email })
+        const user = await User.findOne({ email: req.body.email });
 
         // confere se o usuario existe
-        if(user) {
-            return res.status(422).json({ msg: "Por Favor, utilize outro email!" });
+        if (user) {
+            return res
+                .status(422)
+                .json({ msg: "Por Favor, utilize outro email!" });
         }
 
         // cria um hash para criptografar a senha
@@ -20,20 +22,21 @@ exports.signup = async (req, res, next) => {
         // cria um novo usuario recebendo os dados do usuario antigo + a senha nova criptografada.
         const newUser = await User.create({
             ...req.body,
-                password: hashedPassword,
-        })
+            password: hashedPassword,
+        });
+
+        // secretkey123 - chave secreta para incriptação
 
         // Assign JWT ( Json Web Token ) to user
-        const token = jwt.sign({ _id: newUser._id }, 'secretkey123', {
-            expiresIn: '90d'
-        })
+        const token = jwt.sign({ _id: newUser._id }, "secretkey123", {
+            expiresIn: "90d",
+        });
 
         res.status(201).json({
-            status: 'success',
-            message: 'User registered sucessfully',
-            token
-        })
-
+            status: "success",
+            message: "User registered sucessfully",
+            token,
+        });
     } catch (error) {
         next(error);
     }
@@ -41,5 +44,55 @@ exports.signup = async (req, res, next) => {
 
 // LOGGING USER
 exports.login = async (req, res, next) => {
-    res.send('Login')
+    const { email, password } = req.body;
+
+    // confere os campos preenchidos
+    if (!email) {
+        return res.status(201).json({ msg: "O email é obrigatório!" });
+    }
+    if (!password) {
+        return res.status(201).json({ msg: "A senha é obrigatória!" });
+    }
+
+    // faz uma busca especifica pelo email
+    const user = await User.findOne({ email: req.body.email });
+
+    // confere se o usuario está cadastrado
+    if (!user) {
+        return res.status(422).json({ msg: "Usuairo não cadastrado!" });
+    }
+
+    // checa se a senha está correta
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword) {
+        return res.status(422).json({ msg: "Senha Inválida!"})
+    }
+
+    try{
+
+        const secret = 'secretkey123'
+
+        // verificando token
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            secret, {
+                expiresIn: '90d',
+            }
+        );
+
+        // acima o 3º atributo indica que a chave pode inspirar em 90 dias
+
+        return res.status(200).json({
+            msg: "Autenticação realizada com secesso!", token
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            msg: "AutntiAconteceu um erro no servidor, tente novamente mais tarde!",
+        });
+    }
 };
