4732850c3bebbbec7a898bc464a59a3cd0fe5760
npcwalison <gwalisonls@gmail.com>
2024-06-07 16:24:45 -0300

    finished interface

--- client/src/Auth/Login.jsx
+++ client/src/Auth/Login.jsx
@@ -1,10 +1,116 @@
-import React from 'react';
+import React from "react";
+import { Card, Flex, Form, Input, Button, Alert, Spin, Typography } from "antd";
+import "./styles.css";
+import { Link } from "react-router-dom";
+
+import registerImage from "../assets/register.png";
+
 
 const Login = () => {
-    return(
-        <div>Login</div>
-    )
-}
 
+    const handleLogin = (values) => {
+        console.log(values);
+    };
+
+    const error = false
+    const loading = false
+
+    return (
+        <Card className="form-container">
+            <Flex gap="large" align="center">
+                {/* Image */}
+                <Flex flex={1}>
+                    <img src={registerImage} className="auth-image" />
+                </Flex>
+                {/* form */}
+                <Flex vertical flex={1}>
+                    <Typography.Title level={2} strong className="title">
+                        Sign In
+                    </Typography.Title>
+                    <Typography.Title
+                        type="secondary"
+                        strong
+                        className="slogan"
+                        level={4}
+                    >
+                        Unlock you world!
+                    </Typography.Title>
+                    <Form
+                        layout="vertical"
+                        onFinish={handleLogin}
+                        autoComplete="off"
+                    >
+
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "please input your email!",
+                                },
+                                {
+                                    type: "email",
+                                    message: "The input is not valid email!",
+                                },
+                            ]}
+                        >
+                            <Input
+                                size="large"
+                                placeholder="Enter your full email"
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "please input your passoword!",
+                                },
+                            ]}
+                        >
+                            <Input.Password
+                                size="large"
+                                placeholder="Enter your full password"
+                            />
+                        </Form.Item>
+
+                        {error && (
+                            <Alert
+                                description={error}
+                                type="error"
+                                showIcon
+                                closable
+                                className="alert"
+                            />
+                        )}
+
+                        <Form.Item>
+                            <Button
+                                type={`${loading ? '' : 'primary'}`}
+                                htmlType="submit"
+                                size="large"
+                                className="btn"
+                            >
+                                {loading ? <Spin /> : 'Sign In'}
+                            </Button>
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Link to={"/"}>
+                                <Button size="large" className="btn">
+                                    Create an account
+                                </Button>
+                            </Link>
+                        </Form.Item>
+                    </Form>
+                </Flex>
+
+            </Flex>
+        </Card>
+    );
+};
 
 export default Login;
--- client/src/Auth/Register.jsx
+++ client/src/Auth/Register.jsx
@@ -1,24 +1,33 @@
 import React from "react";
-import { Card, Flex, Form, Input, Typography } from "antd";
+import { Card, Flex, Form, Input, Button, Alert, Spin, Typography } from "antd";
 import "./styles.css";
+import { Link } from "react-router-dom";
+
+import registerImage from "../assets/register.png";
+
 
 const Register = () => {
+
     const handleRegister = (values) => {
         console.log(values);
     };
 
+    const error = false
+    const loading = false
+
     return (
-        <Card className="form-conatiner">
-            <Flex>
+        <Card className="form-container">
+            <Flex gap="large" align="center">
                 {/* form */}
-                <Flex>
-                    <Typography.Title level={3} strong className="title">
+                <Flex vertical flex={1}>
+                    <Typography.Title level={2} strong className="title">
                         Create an account
                     </Typography.Title>
                     <Typography.Title
                         type="secondary"
                         strong
                         className="slogan"
+                        level={4}
                     >
                         Join for exclusive acess!
                     </Typography.Title>
@@ -33,16 +42,104 @@ const Register = () => {
                             rules={[
                                 {
                                     required: true,
-                                    message: 'please input your name!'
-                                }
+                                    message: "please input your name!",
+                                },
                             ]}
                         >
-                            <Input placeholder="Enter your full name" />
+                            <Input
+                                size="large"
+                                placeholder="Enter your full name"
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "please input your email!",
+                                },
+                                {
+                                    type: "email",
+                                    message: "The input is not valid email!",
+                                },
+                            ]}
+                        >
+                            <Input
+                                size="large"
+                                placeholder="Enter your full email"
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "please input your passoword!",
+                                },
+                            ]}
+                        >
+                            <Input.Password
+                                size="large"
+                                placeholder="Enter your full password"
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Password"
+                            name="passwordComfirm"
+                            rules={[
+                                {
+                                    required: true,
+                                    message:
+                                        "please input your confirm password!",
+                                },
+                            ]}
+                        >
+                            <Input.Password
+                                size="large"
+                                placeholder="Confirm your password"
+                            />
+                        </Form.Item>
+
+                        {error && (
+                            <Alert
+                                description={error}
+                                type="error"
+                                showIcon
+                                closable
+                                className="alert"
+                            />
+                        )}
+
+                        <Form.Item>
+                            <Button
+                                type={`${loading ? '' : 'primary'}`}
+                                htmlType="submit"
+                                size="large"
+                                className="btn"
+                            >
+                                {loading ? <Spin /> : 'Create Account'}
+                            </Button>
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Link to={"/login"}>
+                                <Button size="large" className="btn">
+                                    Sign In
+                                </Button>
+                            </Link>
                         </Form.Item>
                     </Form>
                 </Flex>
 
                 {/* Image */}
+                <Flex flex={1}>
+                    <img src={registerImage} className="auth-image" />
+                </Flex>
             </Flex>
         </Card>
     );
